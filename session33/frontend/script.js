f06194bcca075f5b7f10a5efa8b508d79e933d47
Les Carbonaro <lcarbonaro@gmail.com>
2016-08-21 20:38:49 +0000

    updated

--- session33/frontend/script.js
+++ session33/frontend/script.js
@@ -1,56 +1,52 @@
 $(document).ready(function(){
-    
-    // change this to your own server url
-    var serverUrl = 'http://example-lcarbonaro.c9users.io';
 
-    $.get( serverUrl + '/data', function(resp){        
-        
-        var $div = $('div#content');
-        $div.append('<h1>Student List</h1>');
-        var $list = $('<ul>');       
-        
-        resp.forEach( function(item){
-            $list.append('<li>'+item.name+' ('+item.major+')</li>');
-        });
-        
-        $list.appendTo($div);
+	var serverUrl = 'http://example-lcarbonaro.c9users.io';
 
-    });
-    
-    $.get( serverUrl + '/majors', function(resp) {
-        
-        var $div = $('div#content2');
-        var $sel = $('<select>');       
-        
-        $sel.append('<option>Select a major...</option>');
-        
-        resp.forEach( function(item){
-            $sel.append('<option>'+item+'</option>');
-        });
-
-        $sel.appendTo($div);
-        
+    $.get(serverUrl+'/data', function(resp) {
+    	var data =resp;
+    	data.forEach( function(item){
+    		$('div#content').append('<li>'+item.name+' ('+item.major+')</li>');
+    	});
     });
 
-    $('div#content2').on('change','select', function(e){
-    	
-    	var major =  $(this).val();
+    $.get(serverUrl+'/majors', function(resp){
         
-        $.get( serverUrl + '/names/'+major, function(resp){        
-            var $div = $('div#content2');
-            
-            $('div#content2 ul').remove();  // remove any previous list
+        var sel = $('<select>');
+        sel.append('<option>Select a major...</option>')
 
-            var $list = $('<ul>');       
-        
-            resp.forEach( function(item){
-                $list.append('<li>'+item.name+'</li>');
-            });
+        var data = resp;
 
-            $list.appendTo($div);
-        
+        data.forEach( function(item){
+
+        	sel.append('<option>'+item+'</option>')
+
         });
-        
+
+        $('div#content2').append(sel);
+
+
     });
-    
+
+
+    $('div#content2').on('change','select', function(){
+        
+        var selMajor = $(this).val();
+
+        $.get(serverUrl+'/names/'+selMajor, function(resp){
+        	var data =resp;
+        	$('div#content2 ul').remove();
+
+        	var list = $('<ul>');
+
+       	    data.forEach( function(item){
+    		    list.append('<li>'+item.name+'</li>');
+    	    });
+
+    	    $('div#content2').append(list);
+
+        });
+
+    });
+
+
 });
