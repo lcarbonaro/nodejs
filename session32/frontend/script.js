d7a1b533899a0245c3afd77bd50ba0f0d276c401
Les Carbonaro <lcarbonaro@gmail.com>
2016-08-21 20:41:05 +0000

    updated

--- session32/frontend/script.js
+++ session32/frontend/script.js
@@ -1,20 +1,15 @@
 $(document).ready(function(){
-    
-    // change this to your own server address
-    var serverUrl = 'http://example-lcarbonaro.c9users.io';  
-    
-    $.get( serverUrl + '/data', function(resp){        
-        
-        var data = JSON.parse(resp);
-        
-        var $div = $('div#content');
-        $div.append('<h1>Student List</h1>');
-        var $list = $div.append('<ul></ul>');       
-        
-        data.forEach( function(item){
-            $list.append('<li>'+item.name+'</li>');
-        });
-        
+
+    $.get('http://example-lcarbonaro.c9users.io/data', function(resp) {
+
+    	var data = JSON.parse(resp);
+
+    	data.forEach( function(item){
+
+    		$('div#content').append('<li>'+item.name+'</li>');
+
+    	});
+
     });
-    
+
 });
